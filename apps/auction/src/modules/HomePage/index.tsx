898cd71a8b52af93ea59e230445cae71d98374d7
iwoork <efren@iwoork.com>
2023-09-17 22:44:55 -0700

    fix: build errors

--- apps/auction/src/modules/HomePage/index.tsx
+++ apps/auction/src/modules/HomePage/index.tsx
@@ -1,44 +1,8 @@
-import { useState } from 'react';
-
-import { DoorNo, MontyHome, OpenDoorNo } from './components/MontyHome';
-
 export const HomePage = () => {
-  const [openDoorNo, setOpenDoorNo] = useState<OpenDoorNo>(null);
-  const [message, setMessage] = useState<null | string>(null);
-
-  const handleMontyHomeSelected = (doorNo: DoorNo) => {
-    switch (doorNo) {
-      case 1:
-        if (!openDoorNo) {
-          setOpenDoorNo(doorNo);
-          return;
-        }
-        setMessage('Sorry, better luck next time. Baaa 🐐');
-        return;
-      case 2:
-        setMessage('Congrats! 🚗');
-        return;
-      case 3:
-        if (!openDoorNo) {
-          setOpenDoorNo(doorNo);
-          return;
-        }
-        setMessage('Sorry, better luck next time. Baaa 🐐');
-        return;
-    }
-  };
-
   return (
     <div className="m-5">
       <div className="mb-3 text-lg font-medium">Home Page</div>
       {process.env.app_name}
-      <MontyHome
-        title="Home page (scoped) feature"
-        openDoorNo={openDoorNo}
-        message={message}
-        isTiltEnabled={true}
-        onMontyHomeSelected={handleMontyHomeSelected}
-      />
     </div>
   );
 };
